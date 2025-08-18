699ca49112b8316772466e096e6e49e9d48f3fb8
cavera <jgpereirafreitas@gmail.com>
2025-08-18 20:52:01 -0300

    Inclusão classe java usuario

--- src/main/java/com/senac/projeto3/entity/Usuario.java
+++ src/main/java/com/senac/projeto3/entity/Usuario.java
@@ -1,4 +1,53 @@
 package com.senac.projeto3.entity;
 
+import jakarta.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
 public class Usuario {
+    private int id;
+    private String name;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
