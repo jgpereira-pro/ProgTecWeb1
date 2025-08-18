0fe313422b4708964c6e3f67da8345c399595982
cavera <jgpereirafreitas@gmail.com>
2025-08-18 20:20:19 -0300

    jhonny

--- src/main/java/com/senac/projeto3/controller/UsuarioController.java
+++ src/main/java/com/senac/projeto3/controller/UsuarioController.java
@@ -1,26 +1,37 @@
 package com.senac.projeto3.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/usuario")
+@Tag(name="Usuario", description ="API para gerenciamento dos usuarios do sistema")
 public class UsuarioController {
 
-    @GetMapping
+    @GetMapping("/listar")
+    @Operation(summary = "Listar usuarios do sistema")
     public String lister(){
         return "Listando com sucesso";
     }
 
-    @PostMapping
+    @GetMapping("/listarPorIdUsuario{idUsuario}")
+    @Operation(summary = "Listar usuarios do sistema por id do usuario")
+    public String listerPorIdUsuario(@PathVariable("idUsuario") Integer idUsuario){
+        return "Listando um usuario com id "+ idUsuario +" com sucesso";
+    }
+
+    @PostMapping("/atualizar")
     public String criar(){
         return "Usuario criado com sucesso";
     }
 
-    @PutMapping
+    @PutMapping("/criar")
     public String atualizar(){
         return "Usuario atualizado com sucesso";
     }
 
-    @DeleteMapping
+    @DeleteMapping("/apagar")
     public String apagar(){
         return "Usuaria apagado com sucesso";
     }
---
+++ src/main/java/com/senac/projeto3/entity/Usuario.java
@@ -0,0 +1,4 @@
+package com.senac.projeto3.entity;
+
+public class Usuario {
+}
